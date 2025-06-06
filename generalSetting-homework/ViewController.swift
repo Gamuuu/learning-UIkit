c396307065b06805f65d4b4fabe9b0234a520510
Gamuuu <siwakorn.p.1010@gmail.com>
2025-06-06 14:08:13 +0700

    Initial Commit

---
+++ generalSetting-homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  generalSetting-homework
+//
+//  Created by Siwakorn Phanitphichetvong on 6/6/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ generalSetting-homeworkTests/generalSetting_homeworkTests.swift
@@ -0,0 +1,17 @@
+//
+//  generalSetting_homeworkTests.swift
+//  generalSetting-homeworkTests
+//
+//  Created by Siwakorn Phanitphichetvong on 6/6/2568 BE.
+//
+
+import Testing
+@testable import generalSetting_homework
+
+struct generalSetting_homeworkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
